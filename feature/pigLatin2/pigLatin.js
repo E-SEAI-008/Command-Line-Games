3d9af117516422c9ce9ff131b9e8867c5c63e5b4
konstantinos32 <konstantinostheodoropoulosphoto@hotmail.com>
2026-07-20 16:22:49 +0300

    loop that looks if the word starts with a vowell is finished

--- feature/pigLatin2/pigLatin.js
+++ feature/pigLatin2/pigLatin.js
@@ -9,3 +9,14 @@ const words=inputPhrase.split(" ");
 const pigWords=[];
 
 const validVowels=  ["a", "e", "i", "o","u"];
+
+for (let i=0; i<words.length; i++) {
+    const currentWord=words[i];
+    const firstLetter = currentWord[0];
+    const lowerFirstLetter= firstLetter.toLowerCase();
+
+    if(validVowels.includes(lowerFirstLetter)) {
+        pigWords.push(currentWord + "way")
+    }
+
+}

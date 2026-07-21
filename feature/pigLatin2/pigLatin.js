21c893be87c44baf74438a8ed9f1084e3f32225a
konstantinos32 <konstantinostheodoropoulosphoto@hotmail.com>
2026-07-21 09:49:55 +0300

    pig latin game is completed

--- feature/pigLatin2/pigLatin.js
+++ feature/pigLatin2/pigLatin.js
@@ -1,22 +1,62 @@
-const inputPhrase = process.argv(2);
+// Get the phrase from the command line
+const inputPhrase = process.argv[2];
 
-if(!inputPhrase) {
-    console.log('Please give me a phrase that the Pigs can translate')
-    process.exit(1);
+// Check if the user entered a phrase
+if (!inputPhrase) {
+  console.log("Please give me a phrase that the Pigs can translate");
+  process.exit(1);
 }
 
-const words=inputPhrase.split(" ");
-const pigWords=[];
+// Split the sentence into separate words
+const words = inputPhrase.split(" ");
 
-const validVowels=  ["a", "e", "i", "o","u"];
+// Array where we will store the translated words
+const pigWords = [];
 
-for (let i=0; i<words.length; i++) {
-    const currentWord=words[i];
-    const firstLetter = currentWord[0];
-    const lowerFirstLetter= firstLetter.toLowerCase();
+// List of vowels
+const validVowels = ["a", "e", "i", "o", "u"];
 
-    if(validVowels.includes(lowerFirstLetter)) {
-        pigWords.push(currentWord + "way")
+// Go through every word
+for (let i = 0; i < words.length; i++) {
+
+  // Get the current word
+  const currentWord = words[i];
+
+  // Get the first letter of the word
+  const firstLetter = currentWord[0];
+
+  // Make it lowercase only for checking
+  const lowerFirstLetter = firstLetter.toLowerCase();
+
+  // Case 1: Word starts with a vowel
+  if (validVowels.includes(lowerFirstLetter)) {
+
+    pigWords.push(currentWord + "way");
+
+  } else {
+
+    // Get the second letter
+    const secondLetter = currentWord[1];
+
+    // Make it lowercase for checking
+    const lowerSecondLetter = secondLetter.toLowerCase();
+
+    // Case 2: Word starts with two consonants
+    if (!validVowels.includes(lowerSecondLetter)) {
+
+      const newWord = currentWord.slice(2) + currentWord.slice(0, 2) + "ay";
+
+      pigWords.push(newWord);
+
+    } else {
+
+      // Case 3: Word starts with one consonant
+      const newWord = currentWord.slice(1) + currentWord[0] + "ay";
+
+      pigWords.push(newWord);
     }
-
+  }
 }
+
+// Join the translated words back into a sentence
+console.log(pigWords.join(" "));
